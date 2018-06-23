15fd0b143cae2d9f00938e659c1739ebaf49726c
MEA\Aviral.Yadav <aviral.yadav@in.ey.com>
2018-06-23 14:17:44 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url} `;
+    fs.appendFile('server.log', log + '\n', (err) =>{
+        console.log('Unable to log: ', err);
+    });
+    next();
+});
+
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('doUpperText', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.send('Page not found!');
+});
+
+app.listen(2000, () => {
+    console.log('Server is up at port 2000');
+});
